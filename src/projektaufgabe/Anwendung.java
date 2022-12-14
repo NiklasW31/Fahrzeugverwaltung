3c77d75b78dc435ebcd19e63e0635a80bf58b555
lisak <lisak@10.92.25.193>
2022-12-14 10:16:16 +0100

    switchCaseBase

--- src/projektaufgabe/Anwendung.java
+++ src/projektaufgabe/Anwendung.java
@@ -2,11 +2,11 @@ package projektaufgabe;
 
 import java.util.Scanner;
 
-public class Anwendung {
-
-	public static void main(String[] args) {
-		Scanner scnaner = new Scanner(System.in);
-		
+public class Anwendung //Main-Terminal
+{
+	public static void main(String[] args) 
+	{	
+		Scanner scanner = new Scanner(System.in);
 		int auswahl;
 		
 		//Auswahl der Aktion
@@ -16,6 +16,31 @@ public class Anwendung {
 		System.out.println("3. Erstelle ein Fahrzeug");
 		System.out.println("4. Lösche ein Fahrzeug");
 		System.out.println("5. Vergleiche Fahrzeuge");
+		
+		int eingabe = scanner.nextInt();	
+		
+		switch(eingabe) 
+		{
+			case 1:
+				//Gebe eine Liste aller Fahrzeuge aus
+				break;
+			case 2:
+				//Gebe Informationen über ein Fahrzeug aus
+				break;
+			case 3:
+				//Erstelle ein Fahrzeug
+				break;
+			case 4:
+				//Lösche ein Fahrzeug
+				break;
+			case 5:
+				//Vergleiche Fahrzeuge
+				break;
+				
+			default:
+				//Exeption oder simple nachricht
+				break;
+		}
 		
 	}
 
