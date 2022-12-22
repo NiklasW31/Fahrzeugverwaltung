748e5c80e35386f1da0a35a5eae2471b9627d7aa
lisak <lisak@10.92.25.193>
2022-12-22 12:39:32 +0100

    kleine Änderung

--- src/enums/ErlaubtesPersonalBesatzung.java
+++ src/enums/ErlaubtesPersonalBesatzung.java
@@ -1,17 +1,21 @@
 package enums;
 
-public enum ErlaubtesPersonalBesatzung {
+public enum ErlaubtesPersonalBesatzung 
+{	
 	/**
-	 * -> Müssen wir nochmals genauer ansehen, finde nämlich immer andere Namen nach denen die 
-	 *    Fahrzeuge sortiert werden, als diese Typen.
+	 * -> Brauchen wir das überhaupt, ich finde dazu grad irgendwie nichts
 	 * 
-	 * Für Typ A Krankenwagen:
+	 * Rettungswagen: Die personelle Besetzung der Rettungswagen ist im Rettungsdienstgesetz festgelegt. 
+	 * 				  Sie besteht immer aus zwei Personen, davon ein*e Rettungsassistent*in und ein*e 
+	 * 			      Rettungssanitäter*in. Die Rettungsassistentin bzw. der Rettungsassistent übernimmt 
+	 * 				  die Patientenbetreuung und ist der Beifahrer im Fahrzeug. 
 	 * 
-	 * Für TYp B Notfallkrankenwagen:
-	 * 
-	 * Für Typ C Rettungswagen:
-	 * Die personelle Besetzung der Rettungswagen ist im Rettungsdienstgesetz festgelegt. 
-	 * Sie besteht immer aus zwei Personen, davon ein*e Rettungsassistent*in und 
-	 * ein*e Rettungssanitäter*in. 
+	 *  Notarzteinsatzfahrzeug :
+	 *  
+	 *  Krankentransportwagen: Beim Krankentransport muss die Patienten bzw. der Patient gehoben und 
+	 *  					   getragen sowie während der Fahrt durchgehend betreut werden, deshalb 
+	 *  					   besteht die Besatzung immer aus mindestens zwei Personen. 
+	 *  
+	 *  
 	 */
 }
