b2947cb7df19302112311a917bc2732c69ea9790
lisak <lisak@10.92.25.193>
2023-01-11 08:53:26 +0100

    .

--- src/hauptklassen/AusagabeException.java
+++ src/hauptklassen/AusagabeException.java
@@ -2,6 +2,8 @@ package hauptklassen;
 
 public class AusagabeException extends Exception
 {
+	private static final long serialVersionUID = 6004424171245354178L;
+
 	public AusagabeException() {
 		super("Fahrzeugliste konnte nicht ausgegeben werden!");
 	}
