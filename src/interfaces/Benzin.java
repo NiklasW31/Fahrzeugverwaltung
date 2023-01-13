1ffe5cdd4a4a1eaea94e082c49b0c521fbf85c8e
Niklas <108053257+NiklasW31@users.noreply.github.com>
2023-01-13 15:59:01 +0100

    Delete src/interfaces directory
    Interface gelöscht

--- src/interfaces/Benzin.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface Benzin 
-{
-//	public static final String BENZIN = "Diesel"; 
-////	int TANK = 0;
-//	public static final float PREIS = 1.80f;
-////	void benutzeBenzin();
-}
